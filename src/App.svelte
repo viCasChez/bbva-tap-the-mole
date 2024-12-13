4e0a4a7fd5f1561643924b7af2ada97e5990d8b5
David Casco Sánchez <davidcsan@ext.inditex.com>
2024-12-13 11:35:23 +0100

    feat(project): Add BASE_URL in App.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,26 @@
 <script>
+  import { BASE_URL } from './config.js';
   import Home from './routes/Home.svelte';
   import Game from './routes/Game.svelte';
   import { name } from './store'; // Tienda para el nombre
   import { get } from 'svelte/store';
 
   let currentRoute = window.location.pathname;
+  const urlHome = `${BASE_URL}/`;
+  const urlGame = `${BASE_URL}/game`;
 
   // Verificación inicial de la ruta
-  if (currentRoute === '/game' && !get(name)) {
+  if (currentRoute === urlGame && !get(name)) {
     // Redirige a la página principal si no hay nombre
-    currentRoute = '/';
-    history.replaceState({}, '', '/'); // Actualiza la URL en el navegador
+    currentRoute = urlHome;
+    history.replaceState({}, '', urlHome); // Actualiza la URL en el navegador
   }
 
   // Función para manejar la navegación
   function navigate(route) {
-    if (route === '/game' && !get(name)) {
-      currentRoute = '/'; // Redirige a Home si no hay nombre
-      history.pushState({}, '', '/'); // Actualiza la URL
+    if (route === urlGame && !get(name)) {
+      currentRoute = urlHome; // Redirige a Home si no hay nombre
+      history.pushState({}, '', urlHome); // Actualiza la URL
     } else {
       currentRoute = route; // Cambia la ruta actual
       history.pushState({}, '', route); // Actualiza la URL
@@ -26,17 +29,17 @@
 
   // Escucha cambios en el historial del navegador (botones atrás/adelante)
   window.onpopstate = () => {
-    if (window.location.pathname === '/game' && !get(name)) {
+    if (window.location.pathname === urlGame && !get(name)) {
       // Redirige a Home si no hay nombre
-      currentRoute = '/';
-      history.replaceState({}, '', '/');
+      currentRoute = urlHome;
+      history.replaceState({}, '', urlHome);
     } else {
       currentRoute = window.location.pathname;
     }
   };
 
   // Mapea la ruta actual al componente correspondiente
-  $: Component = currentRoute === '/game' ? Game : Home;
+  $: Component = currentRoute === urlGame ? Game : Home;
 </script>
 
 <main>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Home from './routes/Home.svelte';
 import Game from './routes/Game.svelte';
 
 export const routes = {
-  './': Home,
-  './game': Game,
+  '/': Home,
+  '/home': Home,
+  '/game': Game,
 };
