16c226ed50b78362f29474668d60586ee35ea44c
David Casco Sánchez <davidcsan@ext.inditex.com>
2024-12-13 11:18:32 +0100

    feat(project): Add BASE_URL in routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
+import { BASE_URL } from './config.js';
 import Home from './routes/Home.svelte';
 import Game from './routes/Game.svelte';
 
 export const routes = {
-  '/': Home,
-  '/game': Game,
+  [`${BASE_URL}/`]: Home,
+  [`${BASE_URL}/game`]: Game,
 };
